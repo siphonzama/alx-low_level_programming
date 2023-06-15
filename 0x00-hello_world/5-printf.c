ad3efd2ec7b779b0b3fe82ef98cb91a24a7b71da
siphonzama <sipho1728@gmail.com>
2023-06-15 07:32:44 -0700

    num5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,10 @@
-#!/bin/bash
-
+#include <stdio.h>
+/**
+ * Description: main - prints out  a string to stdout.
+ * return 0 if success
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
