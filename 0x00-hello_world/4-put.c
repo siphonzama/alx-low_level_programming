a80f76bbc6ef1ff469a3443d443e8a32fa483720
siphonzama <sipho1728@gmail.com>
2023-06-15 06:51:10 -0700

    num 4

---
+++ 0x00-hello_world/4-put.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+ * description: main - print out string to stdout.
+ * Return: 0 is success.
+ */
+int main(vold)
+{
+puts ("\"programing is like building a multilingual puzzle");
+Return (0);
+}
