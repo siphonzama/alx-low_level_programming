fd3bd50bb62608c657f0256d5fad6068116358d2
siphonzama <sipho1728@gmail.com>
2023-07-31 16:06:55 -0700

    corrected

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -2,19 +2,19 @@
 
 /**
  * a function that returns the number of elements in a linked
- * @head: Pointer to the head od list 
- * Return: Number  of nodes 
+ * @head: Pointer to the head od list
+ * Return: Number  of nodes
  */
 
 size_t listint_len(const listint_t *h)
 {
-	size_t num =0;
+	size_t num = 0;
 
-	while(h)
+	while (h)
 	{
 		num++;
-		h=h->next;
+		h = h->next;
 	}
 
-	return(num);
+	return (num);
 }
