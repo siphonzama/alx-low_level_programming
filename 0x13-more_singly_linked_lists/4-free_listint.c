64dc7846587a88dc837d8c55e3947a87df0e1534
sipho <132463193+siphonzama@users.noreply.github.com>
2023-08-01 17:22:20 +0200

    Update 4-free_listint.c

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -7,12 +7,12 @@
 
 void free_listint(listint_t *head)
 {
-	listint_t *temp;
+	listint_t *holder;
 
 	while (head)
 	{
-		temp = head->next;
+		holder = head->next;
 		free(head);
-		head = temp;
+		head = holder;
 	}
 }
