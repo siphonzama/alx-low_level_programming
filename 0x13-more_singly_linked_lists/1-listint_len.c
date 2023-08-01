6b8d47bcdc480b504f7aac3ddaf7f89d9d7a0e63
sipho <132463193+siphonzama@users.noreply.github.com>
2023-08-01 17:09:44 +0200

    Update 1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -8,13 +8,13 @@
 
 size_t listint_len(const listint_t *h)
 {
-	size_t count num = 0;
+	size_t count = 0;
 
 	while (h)
 	{
-		count num++;
+		count++;
 		h = h->next;
 	}
 
-	return (count num);
+	return (count);
 }
