17a995a2112281171bc01ae6f60e84492178727d
sipho <132463193+siphonzama@users.noreply.github.com>
2023-08-01 17:26:10 +0200

    Update 5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,16 +7,16 @@
 
 void free_listint2(listint_t **head)
 {
-	listint_t *temp;
+	listint_t *holder;
 
 	if (head == NULL)
 		return;
 
 	while (*head)
 	{
-		temp = (*head)->next;
+		holder = (*head)->next;
 		free(*head);
-		*head = temp;
+		*head = holder;
 	}
 
 	*head = NULL;
