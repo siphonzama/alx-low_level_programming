62874cbf5244e2d92e5e6b41d451504bd2688fad
siphonzama <sipho1728@gmail.com>
2023-08-02 01:39:53 -0700

    correction

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -6,14 +6,14 @@
  * Return: the number of nodes
 */
 
-size_t print_listint(const listint_t *h);
+size_t print_listint(const listint_t *h)
 {
 	size_t a = 0;
 
 	while (h != NULL)
 	{
-		printf("%u\n", h->n)
-		h = h - next
+		printf("%u\n", h->n);
+		h = h->next;
 		a++;
 
 	}
