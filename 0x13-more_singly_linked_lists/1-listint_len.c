1ef70c4420d6deba986c06c111dcd3d5af0fae75
siphonzama <sipho1728@gmail.com>
2023-08-02 08:21:58 -0700

    task2

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,2 +1,19 @@
 #include "lists.h"
 
+/**
+ * listint_len -return the number of elements 
+ * @h: used to traverse the linked list 
+ * Return: the number of nodes
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+	while (h)
+	{
+		count++;
+		h = h->next;
+	}
+
+	return (count);
+}
