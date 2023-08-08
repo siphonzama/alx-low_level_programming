0795a40f041571198648b706805df5abfd3e4f85
siphonzama <sipho1728@gmail.com>
2023-08-08 05:07:46 -0700

    retry

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * new_file-give 1024 bytes for a buffer
+ * @file: the name of the file buffer is stores char for
+ *
+ * Return: a pointer to the new
+ */
+
+char *new_file(char *file);
+void close_file(int nf);
+char *new_file(char *file);
+
+{
+	char *file;
+
+	file = mall
