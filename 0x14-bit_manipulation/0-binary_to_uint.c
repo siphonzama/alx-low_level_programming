9abfe7cb9c609a2cfcb26d189e4dddc047ffa69c
siphonzama <sipho1728@gmail.com>
2023-09-29 12:31:28 -0700

    task 1

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,17 +6,17 @@
  * Return: the converted number
  */
 
-unsigned int binary_to_uint(const char *b)
+unsigned int binary_to_uint(const char *b);
 {
 	int i;
 	unsigned int dec_val = 0;
 
-	if(!b)
+	if (!b)
 		return (0);
-	for(i = 0; b[i]; i++)
+	for (i = 0; b[i]; i++)
 	{
-		if(b[i] < '0' || b[i] > '1')
-			return(0);
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
 		dec_val = 2 * dec_val + (b[i] - '0');
 	}
 
