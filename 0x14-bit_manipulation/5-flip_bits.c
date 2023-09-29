97e9fe6d3a18153513a8f6989d1df0f4d1fe1002
siphonzama <sipho1728@gmail.com>
2023-09-29 12:42:00 -0700

    task 5

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -9,7 +9,7 @@
  * Return: number of bits to change
  */
 
-unsigned int flip_bits(unsigned long int n, unsigned long int m);
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
 
 {
 	int i, count = 0;
